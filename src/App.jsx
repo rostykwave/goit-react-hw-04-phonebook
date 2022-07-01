d4c67af00ac64cfa44ad100878ed7e47fd2efea5
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-07-01 18:54:44 +0300

    LS fix when it is empty

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ export const App = () => {
   const CONTACTS_lS_KEY = 'savedContacts';
 
   const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem(CONTACTS_lS_KEY ?? ''))
+    JSON.parse(window.localStorage.getItem(CONTACTS_lS_KEY)) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
   );
-  // const [contacts, setContacts] = useState([
-  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ]);
+
   const [filter, setFilter] = useState('');
 
   const addContactHandler = data => {
@@ -56,18 +56,6 @@ export const App = () => {
     console.log('use APP');
     window.localStorage.setItem(CONTACTS_lS_KEY, JSON.stringify(contacts));
   }, [contacts]);
-
-  // useEffect(() => {
-  //   console.log('APP start');
-
-  //   const SavedContacts = JSON.parse(
-  //     window.localStorage.getItem(CONTACTS_lS_KEY)
-  //   );
-  //   console.log(SavedContacts);
-  //   // if (contacts) {
-  //   //       this.setState({ contacts });
-  //   //     }
-  // }, []);
 
   return (
     <Container>
