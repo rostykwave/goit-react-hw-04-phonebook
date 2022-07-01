022595b53b0c2072ae239554d2bf83264602f378
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-07-01 20:23:50 +0300

    own hoocks

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,38 @@ import { Container } from './components/Container';
 import { Filter } from './components/Filter';
 import { SignUpForm } from 'components/HookForm';
 
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+  return [state, setState];
+};
+
 export const App = () => {
   const CONTACTS_lS_KEY = 'savedContacts';
-
-  const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem(CONTACTS_lS_KEY)) ?? [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
+  const defaultContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+  const [contacts, setContacts] = useLocalStorage(
+    CONTACTS_lS_KEY,
+    defaultContacts
   );
+  // const [contacts, setContacts] = useState(
+  //   () =>
+  //     JSON.parse(window.localStorage.getItem(CONTACTS_lS_KEY)) ?? [
+  //       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //     ]
+  // );
 
   const [filter, setFilter] = useState('');
 
@@ -52,10 +73,10 @@ export const App = () => {
 
   const visibleContacts = getVisibleContacts();
 
-  useEffect(() => {
-    console.log('use APP');
-    window.localStorage.setItem(CONTACTS_lS_KEY, JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   console.log('use APP');
+  //   window.localStorage.setItem(CONTACTS_lS_KEY, JSON.stringify(contacts));
+  // }, [contacts]);
 
   return (
     <Container>
@@ -72,89 +93,3 @@ export const App = () => {
     </Container>
   );
 };
-
-// const CONTACTS_lS_KEY = 'savedContacts';
-
-// export class OLDApp extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-//   componentDidMount() {
-//     const contacts = JSON.parse(localStorage.getItem(CONTACTS_lS_KEY));
-
-//     if (contacts) {
-//       this.setState({ contacts });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const { contacts } = this.state;
-
-//     if (prevState.contacts !== contacts) {
-//       localStorage.setItem(CONTACTS_lS_KEY, JSON.stringify(contacts));
-//     }
-//   }
-
-//   addContactHandler = data => {
-//     const { contacts } = this.state;
-//     if (contacts.find(contact => contact.name === data.name)) {
-//       alert(`${data.name} is already in contacts`);
-//       return;
-//     }
-
-//     const contact = {
-//       id: nanoid(),
-//       ...data,
-//     };
-
-//     this.setState(({ contacts }) => ({
-//       contacts: [contact, ...contacts],
-//     }));
-//   };
-
-//   deleteContactHandler = contactId => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//     }));
-//   };
-
-//   onFilterHandler = e => {
-//     this.setState({ filter: e.currentTarget.value });
-//   };
-
-//   getVisibleContacts = () => {
-//     const { contacts, filter } = this.state;
-//     const normalizedFilter = filter.toLowerCase();
-
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizedFilter)
-//     );
-//   };
-
-//   render() {
-//     const { filter } = this.state;
-//     const visibleContacts = this.getVisibleContacts();
-
-//     return (
-//       <Container>
-//         <SignUpForm />
-//         <h1>PhoneBook</h1>
-//         <ContactForm onSubmit={this.addContactHandler} />
-
-//         <h1>Contacts</h1>
-//         <Filter value={filter} onChange={this.onFilterHandler} />
-//         <ContactList
-//           contacts={visibleContacts}
-//           onDeleteContact={this.deleteContactHandler}
-//         />
-//       </Container>
-//     );
-//   }
-// }
